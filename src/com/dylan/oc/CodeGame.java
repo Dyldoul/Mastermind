a1cd67185838e078de0d26f242fd4643789bb9cd
Dylan Munier <dylan.munier68@gmail.com>
2019-03-08 13:57:50 +0100

    - Méthode launchGame dans GodeGame - Programme OK

--- src/com/dylan/oc/CodeGame.java
+++ src/com/dylan/oc/CodeGame.java
@@ -1,10 +1,12 @@
 package com.dylan.oc;
 
 import java.util.Random;
+import java.util.Scanner;
 
 public class CodeGame {
 
     Random r = new Random();//Création de la variable aléatoire
+    Scanner scan = new Scanner(System.in);
 
     private int nombreCaché;
     private int réponse;
@@ -32,11 +34,41 @@ public class CodeGame {
     //Constructeur
 
     public CodeGame() {
-        this.nombreCaché = r.nextInt(9999);
+        this.nombreCaché = r.nextInt(999); //Choix du nombre aléatoire de 0 à 999
     }
 
     //Méthodes
 
+    /**
+     * Lance le jeu du code secret
+     */
+    public void laucnhGame(){
 
+        System.out.println();
+        System.out.println("NOMBRE SECRET ACTIVE");
+        System.out.println("Serez-vous assez fort pour le trouver ?");
+
+        do {
+            System.out.println();
+            System.out.println("Veuillez entrer un nombre :");
+            setRéponse(scan.nextInt());
+
+            if (getRéponse() > getNombreCaché()) {
+
+                System.out.println();
+                System.out.println("Vous êtes au dessus du nombre secret");
+
+            } else if (getRéponse() < getNombreCaché()) {
+
+                System.out.println();
+                System.out.println("Vous êtes en dessous du nombre secret");
+            }
+
+        } while (getRéponse() != getNombreCaché());
+
+        System.out.println();
+        System.out.println("Félicitation vous avez trouvé le nombre secret !");
+
+    }
 }
 
--- src/com/dylan/oc/Main.java
+++ src/com/dylan/oc/Main.java
@@ -1,8 +1,44 @@
 package com.dylan.oc;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
-    }
+
+        Scanner scan = new Scanner(System.in);
+        int choixMenu; //Variable de navigation dans le menu
+        char exit; // Variable pour quitter un jeu ou non a la fin
+
+        do { // Menu de Départ
+            System.out.println("Bienvenue ! Que voulez-vous faire ?");
+            System.out.println("1 - Jouer au jeu du code secret");
+            System.out.println("2 - Quitter");
+            System.out.println("---------------------------------------------------");
+            choixMenu = scan.nextInt();
+
+            switch (choixMenu) {
+
+                case 1:
+
+                    do { // Lancement du jeu 1
+
+                        CodeGame jeu = new CodeGame();
+                        jeu.laucnhGame();
+                        System.out.println();
+                        System.out.println("Voulez-vous recommencer ? O/N");
+                        scan.nextLine();
+                        String str = scan.nextLine();
+                        exit = str.charAt(0);
+
+                    } while (exit == 'N');
+
+                    break;
+
+            }
+        } while (choixMenu != 2);
+
+        System.out.println("Au revoir ^^");
+
+	}
 }
